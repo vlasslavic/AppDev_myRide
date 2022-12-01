74b3a2c5a3593dc6a00a79c1ae70577605fc0702
BakaDobe <anthoniuspowellus@gmail.com>
2022-12-01 16:24:11 -0500

    Almost completely done

---
+++ myRide/app/src/main/java/com/example/myride/RecyclerTaskAdapter.java
@@ -0,0 +1,52 @@
+package com.example.myride;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.myride.models.garageModel;
+import com.example.myride.models.taskModel;
+import com.example.myride.recyclerView.viewHolder;
+
+import java.util.ArrayList;
+
+public class RecyclerTaskAdapter extends RecyclerView.Adapter<viewHolder> {
+    ArrayList<taskModel> mtasks = new ArrayList<>();
+    Context mContext;
+
+    public RecyclerTaskAdapter(ArrayList<taskModel> mtasks, Context mContext) {
+        this.mtasks = mtasks;
+        this.mContext = mContext;
+    }
+
+    @NonNull
+    @Override
+    public viewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemLayoutView = LayoutInflater.from(parent.getContext()).inflate(
+                R.layout.task_cards, parent,false);
+
+        viewHolder viewHolder = new viewHolder(itemLayoutView);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull viewHolder holder, int position) {
+//        Glide.with(mContext);
+//        holder.txtTask.setText(mtasks.get(position).getTask());
+//        holder.txtService.setText(mtasks.get(position).getServiceLocation();
+    }
+
+    @Override
+    public int getItemCount() {
+        return mtasks.size();
+    }
+
+    public interface OnItemClickListener {
+        void onItemClick(taskModel model);
+    }
+}
--- myRide/app/src/main/java/com/example/myride/ToDoList.java
+++ myRide/app/src/main/java/com/example/myride/ToDoList.java
@@ -1,14 +1,52 @@
 package com.example.myride;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.myride.models.taskModel;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
 
 public class ToDoList extends AppCompatActivity {
+    private final FirebaseAuth mAuth= FirebaseAuth.getInstance();
+    FirebaseUser mUser = mAuth.getCurrentUser();
+    DatabaseReference reff= FirebaseDatabase.getInstance().getReference().child("users").child(mAuth.getCurrentUser().getUid());
+
+    Button btnadd;
+    ArrayList<taskModel> tasksList;
+    RecyclerView recyclerView;
+    RecyclerTaskAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_to_do_list);
+        btnadd=findViewById(R.id.buttontask);
+        tasksList = new ArrayList<>();
+        recyclerView = findViewById(R.id.recycler_View2);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new RecyclerTaskAdapter(tasksList ,this);
+        recyclerView.setAdapter(adapter);
+
+        btnadd.setOnClickListener(new View.OnClickListener() {
+            @Override
+
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), CreateTask.class);
+                startActivity(i);
+            }
+
+        });
     }
 }
