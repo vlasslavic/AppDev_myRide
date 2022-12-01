c24a4847f6a3f3960d8f4557b094e55d5d7535c4
BakaDobe <anthoniuspowellus@gmail.com>
2022-12-01 15:43:33 -0500

    task model is completed

--- myRide/app/src/main/java/com/example/myride/models/taskModel.java
+++ myRide/app/src/main/java/com/example/myride/models/taskModel.java
@@ -1,4 +1,49 @@
 package com.example.myride.models;
 
 public class taskModel {
+    String task;
+    String serviceLocation;
+    String company;
+
+    public taskModel() {
+    }
+
+    public taskModel(String task, String serviceLocation, String company) {
+        this.task = task;
+        this.serviceLocation = serviceLocation;
+        this.company = company;
+    }
+
+    public String getTask() {
+        return task;
+    }
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+
+    public String getServiceLocation() {
+        return serviceLocation;
+    }
+
+    public void setServiceLocation(String serviceLocation) {
+        this.serviceLocation = serviceLocation;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    @Override
+    public String toString() {
+        return "taskModel{" +
+                "task='" + task + '\'' +
+                ", serviceLocation='" + serviceLocation + '\'' +
+                ", company='" + company + '\'' +
+                '}';
+    }
 }
