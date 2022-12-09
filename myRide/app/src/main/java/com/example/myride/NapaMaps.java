c17a23b133866dfffa6d3e405e2069555d953772
antho-mastro <anthony.mastronardi2@gmail.com>
2022-12-08 19:39:02 -0500

    Started Napa

---
+++ myRide/app/src/main/java/com/example/myride/NapaMaps.java
@@ -0,0 +1,14 @@
+package com.example.myride;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NapaMaps extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_napa_maps);
+    }
+}
