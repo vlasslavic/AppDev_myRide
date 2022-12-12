8393a201cb790b7bf656ecc291b675045153c975
Veaceslav Vlas <vlasslavic@gmail.com>
2022-12-12 13:38:54 -0500

    Call Working

--- myRide/app/src/main/java/com/example/myride/NapaMaps.java
+++ myRide/app/src/main/java/com/example/myride/NapaMaps.java
@@ -2,13 +2,31 @@ package com.example.myride;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class NapaMaps extends AppCompatActivity {
-
+    Button call;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_napa_maps);
+        call = findViewById(R.id.button_call);
+
+        call.setOnClickListener(new View.OnClickListener() {
+            @Override
+
+            public void onClick(View v) {
+                Intent callIntent = new Intent(Intent.ACTION_DIAL);
+                callIntent.setData(Uri.parse("tel:5143514210"));
+                startActivity(callIntent);
+            }
+
+        });
     }
 }
